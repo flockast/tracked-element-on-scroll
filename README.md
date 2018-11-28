4b2395fa95488011fb790674d2d4fe706642e778
konstantin vlasov <flockast@gmail.com>
2018-11-28 14:44:25 +0300

    tracked func �‍♂ �‍♂️

---
+++ README.md
@@ -0,0 +1,8 @@
+Tracked Element On Scroll
+===========
+> add class on element when it show in viewport
+## Example
+```js
+const element = document.querySelector(".yo");
+trackedElementOnScroll(element, "fadeIn", 25, "fadeOut");
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {HTMLElement} element
+ * @param {string} className - class which added on element when it place inside viewport
+ * @param {int} [percent=0] - how much percent form bottom viewport you need
+ * @param {removeClass} [removeClass=false] - class which added on element when it place outside viewport
+ */
+
+function trackedElementOnScroll(element, className, percent = 0, removeClass = false) {
+    if(element === undefined || className === undefined) return false;
+    window.addEventListener("scroll", () => {
+        const windowHeight = window.innerHeight;
+        const elementFromTop = element.getBoundingClientRect().top;
+        if(elementFromTop - windowHeight * (100 - percent) / 100 < 0) {
+            element.classList.add(className);
+            if(removeClass) element.classList.remove(removeClass);
+        } else {
+            if(removeClass) {
+                element.classList.remove(className);
+                element.classList.add(removeClass);
+            }
+        }
+    })
+}
